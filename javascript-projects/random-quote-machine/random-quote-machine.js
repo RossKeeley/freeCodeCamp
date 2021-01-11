4e39e022433ed12cf7cc195f1cc2e0f2c4512ea6
Ross Keeley <rjkeeley@gmail.com>
2021-01-11 15:05:48 +0000

    Created functional drum machine.

--- javascript-projects/random-quote-machine/random-quote-machine.js
+++ javascript-projects/random-quote-machine/random-quote-machine.js
@@ -1,7 +1,7 @@
 const API = 'https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json';
 
 class App extends React.Component {
-  state = { 
+  state = {
     quotes: [
       {
         quote:"Life isn’t about getting and having, it’s about giving and being.",
@@ -10,7 +10,7 @@ class App extends React.Component {
     ],
     index: 0
   };
-  
+
   componentDidMount() {
     // call the API and update state
     fetch(API).then(res => res.json())
@@ -20,10 +20,10 @@ class App extends React.Component {
         }, this.getRandomIndex);
     })
   };
-  
+
   getRandomIndex = () => {
     const {quotes} = this.state;
-    
+
     if(quotes.length > 0) {
       const index = Math.floor(Math.random() * quotes.length);
       this.setState({
@@ -31,17 +31,17 @@ class App extends React.Component {
       })
     }
   }
-  
+
   render() {
     const {quotes, index} = this.state;
-    
+
     const quote = quotes[index];
-    
+
     const tweetURL = 'https://twitter.com/intent/tweet?text=${quote.quote} - ${quote.author}';
-    
+
     return (
       <div id="quote-box" className="wrapper d-flex align-items-center justify-content-center">
-        
+
         <div className="col-6 box p-4 rounded">
           {
             quote && (
@@ -50,14 +50,14 @@ class App extends React.Component {
                   "{quote.quote}"
                 </h5>
                 <cite id="author" className="d-block text-right">- {quote.author}</cite>
-              </div>  
+              </div>
             )
-          }          
+          }
           <div className="d-flex justify-content-between">
             <a id="tweet-quote" className="btn btn-primary" target="_blank" href={tweetURL}>Tweet</a>
             <button id="new-quote" className="btn btn-primary" onClick={this.getRandomIndex}>Get Quote</button>
           </div>
-          
+
         </div>
       </div>
     )
