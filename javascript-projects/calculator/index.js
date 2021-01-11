b621c1bcbd9ce9f5eba0f2537f628beecc8a68c6
Ross Keeley <rjkeeley@gmail.com>
2021-01-11 20:36:56 +0000

    Added output to display console

--- javascript-projects/calculator/index.js
+++ javascript-projects/calculator/index.js
@@ -3,15 +3,18 @@
 // Function to clear display using clear (AC) button
 document.getElementById("clear").addEventListener("click", () => {
   document.getElementById("display").innerHTML = "0";
+  document.getElementById("output").innerHTML = "";
 });
 
-// Function to add numbers to the display
+// Function to commit numbers to the display
 function inputNumber(number, value) {
   document.getElementById(number).addEventListener("click", () => {
     if (document.getElementById("display").innerHTML === "0") {
       document.getElementById("display").innerHTML = "";
+      document.getElementById("output").innerHTML = "";
     }
     document.getElementById("display").innerHTML += value;
+    document.getElementById("output").innerHTML += value;
   })
 }
 
@@ -27,9 +30,12 @@ inputNumber("seven", 7);
 inputNumber("eight", 8);
 inputNumber("nine", 9);
 
+// Function to commit symbols to the display
 function inputSymbol(symbol, value) {
   document.getElementById(symbol).addEventListener("click", () => {
     document.getElementById("display").innerHTML += value;
+    document.getElementById("output").innerHTML += value;
+
   })
 }
 
@@ -38,5 +44,21 @@ inputSymbol("divide", " / ");
 inputSymbol("multiply", " * ");
 inputSymbol("subtract", " - ");
 inputSymbol("add", " + ");
-inputSymbol("equals", " = ");
 inputSymbol("decimal", ".");
+
+// Perform calculations
+// Add event listener to equals button to perform calculation on display inner html
+// document.getElementById("equals").addEventListener("click", () => {
+//   document.getElementById("display")innerHTML = eval(document.getElementById("output")innerHTML);
+// });
+
+
+// When the decimal element is clicked, a "." should append to the currently displayed value; two "." in one number should not be accepted
+
+// I should be able to perform any operation (+, -, *, /) on numbers containing decimal points
+
+// If 2 or more operators are entered consecutively, the operation performed should be the last operator entered (excluding the negative (-) sign
+
+// Pressing an operator immediately following "=" should start a new calculation that operates on the result of the previous evaluation
+
+// My calculator should have several decimal places of precision when it comes to rounding (note that there is no exact standard, but you should be able to handle calculations like 2 / 7 with reasonable precision to at least 4 decimal places).
