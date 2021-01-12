f8ea13383dd40d171b2226ca6b0babc450f9778d
Ross Keeley <rjkeeley@gmail.com>
2021-01-12 11:50:28 +0000

    Solved only 1 decimal allowed in output and only left of nums in display

--- javascript-projects/calculator/index.js
+++ javascript-projects/calculator/index.js
@@ -6,7 +6,7 @@ document.getElementById("clear").addEventListener("click", () => {
   document.getElementById("output").innerHTML = "";
 });
 
-// Function to commit numbers to the display
+// Function to commit numbers to the display console
 function inputNumber(number, value) {
   document.getElementById(number).addEventListener("click", () => {
     if (document.getElementById("display").innerHTML === "0" || (document.getElementById("output").innerHTML).indexOf("=") > -1) {
@@ -30,7 +30,7 @@ inputNumber("seven", 7);
 inputNumber("eight", 8);
 inputNumber("nine", 9);
 
-// Function to commit symbols to the display
+// Function to commit symbols to the display console
 function inputSymbol(symbol, value) {
   document.getElementById(symbol).addEventListener("click", () => {
     if (document.getElementById("output").innerHTML.indexOf("=") > -1) {
@@ -48,7 +48,16 @@ inputSymbol("divide", " / ");
 inputSymbol("multiply", " * ");
 inputSymbol("subtract", " - ");
 inputSymbol("add", " + ");
-inputSymbol("decimal", ".");
+
+// Commit decimal symbol to the display console
+document.getElementById("decimal").addEventListener("click", () => {
+  if (document.getElementById("display").innerHTML.indexOf(".") < 0) {
+    document.getElementById("output").innerHTML += document.getElementById("decimal").innerHTML;
+    document.getElementById("display").innerHTML += document.getElementById("decimal").innerHTML;
+  };
+});
+
+// inputSymbol("decimal", ".");
 
 // Perform calculations
 // Add event listener to equals button to perform calculation on display inner html
@@ -59,10 +68,8 @@ document.getElementById("equals").addEventListener("click", () => {
 });
 // When the decimal element is clicked, a "." should append to the currently displayed value; two "." in one number should not be accepted
 
-// I should be able to perform any operation (+, -, *, /) on numbers containing decimal points
+// Numbers and symbols should appear seperately in the display, not together. This does not include decimals
 
-// If 2 or more operators are entered consecutively, the operation performed should be the last operator entered (excluding the negative (-) sign
+// No more than one decimal point should appear per number i.e. only one on the display, but more than one should be allowed on the output
 
-// Pressing an operator immediately following "=" should start a new calculation that operates on the result of the previous evaluation
-
-// My calculator should have several decimal places of precision when it comes to rounding (note that there is no exact standard, but you should be able to handle calculations like 2 / 7 with reasonable precision to at least 4 decimal places).
+// The display should allow for decimals to appear to the right and left of numbers on the display
