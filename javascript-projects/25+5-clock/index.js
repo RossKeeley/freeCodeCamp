3ecfcf920f866e397449a09db73296bcc82f79a8
Ross Keeley <rjkeeley@gmail.com>
2021-01-16 17:24:12 +0000

    Added App class component, and css file

---
+++ javascript-projects/25+5-clock/index.js
@@ -0,0 +1,8 @@
+const audio = document.getElementById("beep");
+
+// Class component containing all app components and states
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+}
