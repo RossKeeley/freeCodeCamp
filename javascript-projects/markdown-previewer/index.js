633555076511db5629016ee204fb65befcdacf7a
Ross Keeley <rjkeeley@gmail.com>
2021-01-18 19:10:03 +0000

    Added css file with styling and a clear text button to the input

--- javascript-projects/markdown-previewer/index.js
+++ javascript-projects/markdown-previewer/index.js
@@ -1,8 +1,17 @@
+const exampleText = "";
+
 marked.setOptions({breaks: true});
 var input = document.getElementById("editor");
+var preview = document.getElementById("preview");
 
-document.getElementById("preview").innerHTML = marked(document.getElementById("editor").value);
+preview.innerHTML = marked(input.value);
 
 input.addEventListener("keyup", () => {
-  document.getElementById("preview").innerHTML = marked(document.getElementById("editor").value);
+  preview.innerHTML = marked(input.value);
+});
+
+// Clear button
+document.getElementById("clearButton").addEventListener("click", () => {
+  input.value = "";
+  preview.innerHTML = "";
 });
