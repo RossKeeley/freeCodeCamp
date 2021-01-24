6f4194e20474779675ad359acca932164880156c
Ross Keeley <rjkeeley@gmail.com>
2021-01-24 18:02:39 +0000

    Reverting code - saved sort code to wrong file

--- javascript-exercises/implement-bubble-sort.js
+++ javascript-exercises/implement-bubble-sort.js
@@ -1,11 +1,13 @@
-function selectionSort(array) {
-  var result = [];
-  do {
-    result.push(Math.min(...array));
-    array.splice(array.indexOf(Math.min(...array)), 1)
-  } while (array.length > 0);
-  return result;
+function bubbleSort(array) {
+  for (let i = 0; i < array.length; i++) {
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[i] > array[j]) {
+        array.splice(i, 0, array[j]);
+        array.splice(j + 1, 1);
+      }
+    }
+  }
+  return array;
 }
 
-
-console.log(selectionSort([1, 4, 2, 8, 345, 123, 43, 32, 5643, 63, 123, 43, 2, 55, 1, 234, 92]));
+console.log(bubbleSort([1, 4, 2, 8, 345, 123, 43, 32, 5643, 63, 123, 43, 2, 55, 1, 234, 92]));
