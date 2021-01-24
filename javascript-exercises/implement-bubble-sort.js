46cb3be28177ab9293bd894cd1e7b06532c05414
Ross Keeley <rjkeeley@gmail.com>
2021-01-24 16:57:49 +0000

    Completed freecodecamp javascript problem

---
+++ javascript-exercises/implement-bubble-sort.js
@@ -0,0 +1,13 @@
+function bubbleSort(array) {
+  for (let i = 0; i < array.length; i++) {
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[i] > array[j]) {
+        array.splice(i, 0, array[j]);
+        array.splice(j + 1, 1);
+      }
+    }
+  }
+  return array;
+}
+
+console.log(bubbleSort([1, 4, 2, 8, 345, 123, 43, 32, 5643, 63, 123, 43, 2, 55, 1, 234, 92]));
